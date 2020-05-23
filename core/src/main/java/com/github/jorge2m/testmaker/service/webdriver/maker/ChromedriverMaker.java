74340545b2999899f1980aa7dceb704498cb2b5f
Jorge Munoz <jorge.munoz.sge@mango.com>
2020-05-23 10:34:22 +0200

    Migrate from Chrome81 to Chrome83

--- core/src/main/java/com/github/jorge2m/testmaker/service/webdriver/maker/ChromedriverMaker.java
+++ core/src/main/java/com/github/jorge2m/testmaker/service/webdriver/maker/ChromedriverMaker.java
@@ -24,7 +24,7 @@ class ChromedriverMaker extends DriverMaker {
 	
 	//La versión de ChromeDriver ha de soportar la versión de Chrome instalada en el servidor donde se ejecute TestMaker
 	private final boolean isHeadless;
-	private final static String ChromeDriverVersionDefault = "81.0.4044.69";
+	private final static String ChromeDriverVersionDefault = "83.0.4103.39";
 	private ChromeOptions options = new ChromeOptions();
 	
 	public ChromedriverMaker(boolean isHeadless) {
