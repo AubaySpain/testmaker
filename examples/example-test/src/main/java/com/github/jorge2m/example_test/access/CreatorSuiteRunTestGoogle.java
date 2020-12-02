1436ef9ed268983d9060f3173b942f891c166af6
Jorge Munoz <jorge.munoz.sge@mango.com>
2020-12-02 17:47:15 +0100

    #Ajustes

--- examples/example-test/src/main/java/com/github/jorge2m/example_test/access/CreatorSuiteRunTestGoogle.java
+++ examples/example-test/src/main/java/com/github/jorge2m/example_test/access/CreatorSuiteRunTestGoogle.java
@@ -10,7 +10,7 @@ import com.github.jorge2m.testmaker.domain.SuiteMaker;
 
 public class CreatorSuiteRunTestGoogle extends CreatorSuiteRun {
 
-	private final String ChromeDriverVersionDefault = "85.0.4183.83";
+	private final String ChromeDriverVersionDefault = "87.0.4280.20";
 	private final String GeckoDriverVersionDefault = "0.27.0";
 	
 	private CreatorSuiteRunTestGoogle() throws Exception {
